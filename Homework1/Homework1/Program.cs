37f9f39cf7ecc351106428b9b7b9d1da4793fa84
2025513497 <2025513497@qq.com>
2018-09-19 11:51:17 +0800

    第一次提交

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            string a = Console.ReadLine();
+            Console.Write("b=");
+            string b = Console.ReadLine();
+            double x = Convert.ToDouble(a);
+            double y = Convert.ToDouble(b);
+            Console.WriteLine("a*b=" + (x * y));
+            Console.ReadKey();
+        }
+    }
+}
